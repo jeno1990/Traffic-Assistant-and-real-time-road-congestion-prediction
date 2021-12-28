28dd35e12ba157018c289e8263eef512b992d2eb
Nigatu Paulos <nigatujeno@gmail.com>
2021-12-28 17:04:49 +0300

    frontend folder structure restructed

---
+++ frontend/mobile_traffic/lib/screens/signup/signup.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatelessWidget {
+  // const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //continue signup
+      ),
+    );
+  }
+}
