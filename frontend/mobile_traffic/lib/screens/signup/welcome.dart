949f42d7d92eb3fb00d66fa753d7de6f8ad326c2
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2021-12-30 20:04:58 +0300

    get dart

--- frontend/mobile_traffic/lib/screens/signup/welcome.dart
+++ frontend/mobile_traffic/lib/screens/signup/welcome.dart
@@ -1,45 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_traffic/screens/common_components/Button.dart';
 import 'package:mobile_traffic/screens/common_components/header_text.dart';
+import 'package:get/get.dart';
+import 'package:mobile_traffic/screens/signup/login.dart';
+
 class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          body:Stack(
-                        children: [
-        Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: FractionalOffset.topCenter,
-                  end: FractionalOffset.bottomCenter,
-                  colors: [
-                Color.fromRGBO(72, 131, 246, 0.7),
-                Color.fromRGBO(0, 0, 0, 0.7)
-              ],
-                  stops: [
-                0.0,
-                1.0
-              ])),
-        ),
-        Container(
-          padding: EdgeInsets.all(50),
-          child: Column(
-            crossAxisAlignment:CrossAxisAlignment.center,
-            children: [
-            
+        child: Scaffold(
+            body: Stack(children: [
+      Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+              Color.fromRGBO(72, 131, 246, 0.7),
+              Color.fromRGBO(0, 0, 0, 0.7)
+            ],
+                stops: [
+              0.0,
+              1.0
+            ])),
+      ),
+      Container(
+        padding: EdgeInsets.all(50),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
           HeaderText("Welcome"),
-       Button('Office', (){}, Color.fromRGBO(31, 36, 158, 0.42)),
-       Button('Car Owner', (){}, Color.fromRGBO(31, 36, 158, 0.42)),
-       Button('Traffic Police', (){}, Color.fromRGBO(31, 36, 158, 0.42)),
-       Button('As Gust', (){}, Color.fromRGBO(31, 36, 158, 0.42)),
-
-      ]),
-        )
-       ] )));
-          
-          
-           
-    
+          Button('Office', () => {Get.to(Login())},
+              Color.fromRGBO(31, 36, 158, 0.42)),
+          Button('Car Owner', () => {Get.to(Login())}
+          , Color.fromRGBO(31, 36, 158, 0.42)),
+          Button('Traffic Police', () => {Get.to(Login())}
+          , Color.fromRGBO(31, 36, 158, 0.42)),
+          Button('As Gust', () => {Get.to(Login())}
+          , Color.fromRGBO(31, 36, 158, 0.42)),
+        ]),
+      )
+    ])));
   }
 }
