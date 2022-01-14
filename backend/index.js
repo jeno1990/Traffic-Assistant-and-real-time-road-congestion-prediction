410c5b50b0782135221c7fc9fb8e92bbf6c89259
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2022-01-14 15:01:39 +0300

    using handler function to driver signup and lognin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost:27017/FinalProject'
 const connection =mongoose.connection;
 connection.once("open",()=>console.log('mongodb connected'));
 app.use(express.json());
-const userRoute =require('./routes/driver/driver_login');
+const userRoute =require('./routes/driver.route');
 app.use("/driver",userRoute);
 app.route("/").get((req,res)=>res.json('your first rest ap2i'));
 app.listen(port,()=>console.log(`your server is running in port ${port}`));
