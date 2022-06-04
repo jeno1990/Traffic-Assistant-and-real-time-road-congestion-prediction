64fa32765e0a2d475cf76579e17b518a17fdf89e
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2022-06-04 23:04:06 +0300

    traffic notification controller  added

--- frontend/mobile_traffic/lib/controllers/traffic_conrollers/traffic_notificationController.dart
+++ frontend/mobile_traffic/lib/controllers/traffic_conrollers/traffic_notificationController.dart
@@ -1,18 +1,60 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 import 'package:mobile_traffic/models/notification_model.dart';
+import 'package:mobile_traffic/models/traffic_notification_model.dart';
 import 'package:mobile_traffic/services/api_service.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class TrafficNotificationController extends GetxController {
+  late IO.Socket socket;
+  double? latitude;
+  double? longtude;
+
   // ignore: deprecated_member_use
   var notificationList = List<NotificationModel>.empty(growable: true).obs;
   var isLoading = true.obs;
+  var online = true;
   @override
   void onInit() {
     print("hhhh" + notificationList.toString());
 
     getNotification();
+    //trafficDashboard;
+    initSocket();
     update();
     super.onInit();
+  }
+
+  void inActive() {
+    socket.disconnect();
+    // to check if the traffic is online or offline
+  }
+
+  var notificationTrafficList =
+      List<TrafficNotification>.empty(growable: true).obs;
+
+  Future<void> initSocket() async {
+    try {
+      socket = IO.io("http://10.5.227.228:5000", <String, dynamic>{
+        "transports": ["websocket"],
+        "autoConnect": true,
+      });
+      socket.on("userCount", (data) async {
+        print(data);
+    
+        final trafficNotification = await TrafficNotification.fromJson(data);
+        notificationTrafficList.add(trafficNotification);
+        update();
+
+        print(notificationTrafficList.length);
+      });
+      socket.connect();
+      socket.onConnect((data) => print('Connected: ${socket.id}'));
+      print(socket.connected);
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   void getNotification() async {
